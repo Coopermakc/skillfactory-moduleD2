a356b6963a7ca01addfd432b19db56d2499967ad
Coopermakc <Coopermakc@mail.ru>
2020-06-19 12:01:29 +0300

    init commit

---
+++ server.py
@@ -0,0 +1,44 @@
+import sentry_sdk
+from bottle import route, HTTPResponse, run
+from sentry_sdk.integrations.bottle import BottleIntegration
+
+import os
+
+sentry_sdk.init(
+    dsn="https://e59da7e3bd844d42b45a14a590f19461@o409683.ingest.sentry.io/5282734",
+    integrations=[BottleIntegration()]
+    )
+
+
+@route("/success")
+def success():
+    html = """
+        <!doctype html>
+        <html lang="en">
+        <head>
+            <title>Генератор утверждений</title>
+        </head>
+        <body>
+            <div class="container">
+            <h1>Success page!</h1>
+            <p class="small">Эта страница не возвращает ошибок</p>
+            </div>
+        </body>
+        </html>
+        """
+    return HTTPResponse(status=200, body=html)
+
+@route("/fail")
+def fail():
+    return HTTPResponse(status=500, body='Here error with code 500')
+
+
+if os.environ.get("APP_LOCATION") == "heroku":
+    run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        server="gunicorn",
+        workers=3,
+    )
+else:
+    run(host="localhost", port=8080, debug=True)
